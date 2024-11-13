3ac112012a082c7136ca8660cd22da691750a7d7
Candela Guadalupe Godoy <163220967+candelagodoy@users.noreply.github.com>
2024-11-13 16:58:09 -0300

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.static('public'));
 
 app.use("/turno", turnoRouter )
 app.use("/historiaClinica", historiaClinicaRouter )
-app.use("/agenda", agendaRouter)
+app.use("/", agendaRouter)
 app.use("/consulta", consultaRouter)
 
 
