40fd022a5cb38ea1010079df55ed13283435249f
Candela Guadalupe Godoy <163220967+candelagodoy@users.noreply.github.com>
2024-11-13 19:52:24 -0300

    .

--- index.js
+++ index.js
@@ -7,15 +7,18 @@ const agendaRouter = require('./routes/agenda');
 const consultaRouter = require("./routes/consulta")
 const path = require('path');
 
+app.use(express.urlencoded({extended:true}))
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 //app.set('views', './views');
 
 app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
+app.use("/", agendaRouter)
 app.use("/turno", turnoRouter )
 app.use("/historiaClinica", historiaClinicaRouter )
-app.use("/", agendaRouter)
 app.use("/consulta", consultaRouter)
 
 
